d2a000e915a735edfc6216ccf4c88c15055c1f4c
moderncrazy <geekeryoung.gao@softtek.com>
2020-03-31 16:55:43 +0800

    update send message parameter

--- src/router/messageRouter.js
+++ src/router/messageRouter.js
@@ -5,6 +5,7 @@
  */
 
 const util = require('../util/util');
+const handleFrom = require('../middleware/handleFrom');
 const parameterValidate = require('../middleware/parameterValidate');
 
 /**
@@ -13,6 +14,22 @@ const parameterValidate = require('../middleware/parameterValidate');
  * @param puppet
  */
 module.exports = async (router, puppet) => {
+
+  const FROM_RULE = {
+    id: {type: 'string'},
+    name: {type: 'string'},
+    avatar: {type: 'string'},
+    reason: {type: 'string'},
+    type: {type: 'enum', values: [0, 1, 2]},
+    gender: {type: 'enum', values: [0, 1, 2]},
+    city: {type: 'string', required: false},
+    alias: {type: 'string', required: false},
+    star: {type: 'boolean', required: false},
+    weixin: {type: 'string', required: false},
+    friend: {type: 'boolean', required: false},
+    address: {type: 'string', required: false},
+    province: {type: 'string', required: false},
+  };
 
   /**
    * send message by room
@@ -24,10 +41,11 @@ module.exports = async (router, puppet) => {
     type: {type: 'enum', values: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]},
     text: {type: 'string', required: false},
     toId: {type: 'string', required: false},
-    fromId: {type: 'string', required: false},
     filename: {type: 'string', required: false},
     timestamp: {type: 'number', required: false},
+    from: {type: 'object', rule: FROM_RULE, required: false},
   }));
+  router.post('/message/room', handleFrom(puppet));
   router.post('/message/room', (ctx) => {
     const {request} = ctx;
     const {timestamp, ...data} = request.body;
@@ -44,14 +62,13 @@ module.exports = async (router, puppet) => {
   router.post('/message/single', parameterValidate({
     id: {type: 'string'},
     toId: {type: 'string'},
-    fromId: {type: 'string'},
-    mentionIdList: {type: 'array', itemType: 'string'},
+    from: {type: 'object', rule: FROM_RULE},
     type: {type: 'enum', values: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]},
     text: {type: 'string', required: false},
-    roomId: {type: 'string', required: false},
     filename: {type: 'string', required: false},
     timestamp: {type: 'number', required: false},
   }));
+  router.post('/message/single', handleFrom(puppet));
   router.post('/message/single', (ctx) => {
     const {request} = ctx;
     const {timestamp, ...data} = request.body;
