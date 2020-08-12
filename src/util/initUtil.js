9ebbb4e74331dc5e8f5275773ebab4e720cdf5b0
moderncrazy <geekeryoung.gao@softtek.com>
2020-08-12 13:24:22 +0800

    update init mock data

--- src/util/initUtil.js
+++ src/util/initUtil.js
@@ -16,7 +16,6 @@ module.exports = {
     const data = JSON.parse(fs.readFileSync(file, {encoding: 'utf8'}));
     await this.importContact([data.login], puppet);
     await this.importContact(data.contactList, puppet);
-    await this.importRoomMember(data.roomMemberList, puppet);
     await this.importRoom(data.roomList, puppet);
     return data;
   },
@@ -24,41 +23,24 @@ module.exports = {
   /**
    * import contact
    * @param contactList {Array}
-   * @param puppet
+   * @param puppet {PuppetMock}
    */
   async importContact(contactList, puppet) {
     // import contact
     for (let contact of contactList) {
-      await puppet.cacheContactPayload.set(contact.id, contact);
-    }
-  },
-
-  /**
-   * import room
-   * @param roomMemberList {Array}
-   * @param puppet
-   */
-  async importRoomMember(roomMemberList, puppet) {
-    // import roomMember
-    for (let roomMember of roomMemberList) {
-      // extract the corresponding contact
-      let contact = (await puppet.cacheContactPayload.get(roomMember.id)) || {};
-      await puppet.cacheRoomMemberPayload.set(roomMember.id, Object.assign({
-        name: contact.name || 'mock_name',
-        avatar: contact.avatar || 'https://avatars0.githubusercontent.com/u/21285357?s=200&v=4'
-      }, roomMember));
+      puppet.mocker.createContact(contact);
     }
   },
 
   /**
    * import room
    * @param roomList {Array}
-   * @param puppet
+   * @param puppet {PuppetMock}
    */
   async importRoom(roomList, puppet) {
     // import room
     for (let room of roomList) {
-      await puppet.cacheRoomPayload.set(room.id, room);
+      puppet.mocker.createRoom(room)
     }
   },
 
